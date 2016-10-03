6669453d3e943a23800165148621dccee4c1ad1b
JacoboCL <j.conejol@alumnos.upm.es>
2016-10-03 18:33:54 +0200

    Updated Conexion class to use State pattern.

--- src/main/java/state/connection/Conexion.java
+++ src/main/java/state/connection/Conexion.java
@@ -1,14 +1,14 @@
 package state.connection;
 
 public class Conexion {
-    private Estado estado;
-
     private Link link;
+    
+    private State state;
 
     public Conexion(Link link) {
         assert link != null;
         this.link = link;
-        this.estado = Estado.CERRADO;
+        //this.estado = Estado.CERRADO;
     }
 
     public Link getLink() {
@@ -16,86 +16,95 @@ public class Conexion {
     }
 
     public Estado getEstado() {
-        return this.estado;
+        return state.getEstado();
     }
 
     public void abrir() {
-        if (this.estado == Estado.CERRADO) {
-            this.estado = Estado.PREPARADO;
-        } else if (this.estado == Estado.PARADO) {
-            throw new UnsupportedOperationException("Acción no permitida... ");
-        } else if (this.estado == Estado.PREPARADO) {
-        } else if (this.estado == Estado.ESPERANDO) {
-            throw new UnsupportedOperationException("Acción no permitida... ");
-        } else
-            assert false : "estado imposible";
+        state.abrir(this);
+//        if (this.estado == Estado.CERRADO) {
+//            this.estado = Estado.PREPARADO;
+//        } else if (this.estado == Estado.PARADO) {
+//            throw new UnsupportedOperationException("Acción no permitida... ");
+//        } else if (this.estado == Estado.PREPARADO) {
+//        } else if (this.estado == Estado.ESPERANDO) {
+//            throw new UnsupportedOperationException("Acción no permitida... ");
+//        } else
+//            assert false : "estado imposible";
     }
 
     public void cerrar() {
-        if (this.estado == Estado.CERRADO) {
-        } else if (this.estado == Estado.PARADO) {
-            throw new UnsupportedOperationException("Acción no permitida... ");
-        } else if (this.estado == Estado.PREPARADO) {
-            this.estado = Estado.CERRADO;
-        } else if (this.estado == Estado.ESPERANDO) {
-            throw new UnsupportedOperationException("Acción no permitida... ");
-        }
-        assert false : "estado imposible";
+        state.cerrar(this);
+//        if (this.estado == Estado.CERRADO) {
+//        } else if (this.estado == Estado.PARADO) {
+//            throw new UnsupportedOperationException("Acción no permitida... ");
+//        } else if (this.estado == Estado.PREPARADO) {
+//            this.estado = Estado.CERRADO;
+//        } else if (this.estado == Estado.ESPERANDO) {
+//            throw new UnsupportedOperationException("Acción no permitida... ");
+//        }
+//        assert false : "estado imposible";
     }
 
     public void parar() {
-        if (this.estado == Estado.CERRADO) {
-            throw new UnsupportedOperationException("Acción no permitida... ");
-        } else if (this.estado == Estado.PARADO) {
-        } else if (this.estado == Estado.PREPARADO) {
-            this.estado = Estado.PARADO;
-        } else if (this.estado == Estado.ESPERANDO) {
-            throw new UnsupportedOperationException("Acción no permitida... ");
-        }
-        assert false : "estado imposible";
+        state.parar(this);
+//        if (this.estado == Estado.CERRADO) {
+//            throw new UnsupportedOperationException("Acción no permitida... ");
+//        } else if (this.estado == Estado.PARADO) {
+//        } else if (this.estado == Estado.PREPARADO) {
+//            this.estado = Estado.PARADO;
+//        } else if (this.estado == Estado.ESPERANDO) {
+//            throw new UnsupportedOperationException("Acción no permitida... ");
+//        }
+//        assert false : "estado imposible";
     }
 
     public void iniciar() {
-        if (this.estado == Estado.CERRADO) {
-            throw new UnsupportedOperationException("Acción no permitida... ");
-        } else if (this.estado == Estado.PARADO) {
-            this.estado = Estado.PREPARADO;
-        } else if (this.estado == Estado.PREPARADO) {
-        } else if (this.estado == Estado.ESPERANDO) {
-            throw new UnsupportedOperationException("Acción no permitida... ");
-        }
-        assert false : "estado imposible";
+        state.iniciar(this);
+//        if (this.estado == Estado.CERRADO) {
+//            throw new UnsupportedOperationException("Acción no permitida... ");
+//        } else if (this.estado == Estado.PARADO) {
+//            this.estado = Estado.PREPARADO;
+//        } else if (this.estado == Estado.PREPARADO) {
+//        } else if (this.estado == Estado.ESPERANDO) {
+//            throw new UnsupportedOperationException("Acción no permitida... ");
+//        }
+//        assert false : "estado imposible";
     }
 
     public void enviar(String msg) {
-        if (this.estado == Estado.CERRADO) {
-            throw new UnsupportedOperationException("Acción no permitida... ");
-        } else if (this.estado == Estado.PARADO) {
-            throw new UnsupportedOperationException("Acción no permitida... ");
-        } else if (this.estado == Estado.PREPARADO) {
-            this.link.enviar(msg);
-            this.estado = Estado.ESPERANDO;
-        } else if (this.estado == Estado.ESPERANDO) {
-            throw new UnsupportedOperationException("Acción no permitida... ");
-        }
-        assert false : "estado imposible";
+        state.enviar(this, msg);
+//        if (this.estado == Estado.CERRADO) {
+//            throw new UnsupportedOperationException("Acción no permitida... ");
+//        } else if (this.estado == Estado.PARADO) {
+//            throw new UnsupportedOperationException("Acción no permitida... ");
+//        } else if (this.estado == Estado.PREPARADO) {
+//            this.link.enviar(msg);
+//            this.estado = Estado.ESPERANDO;
+//        } else if (this.estado == Estado.ESPERANDO) {
+//            throw new UnsupportedOperationException("Acción no permitida... ");
+//        }
+//        assert false : "estado imposible";
     }
 
     public void recibir(int respuesta) {
-        if (this.estado == Estado.CERRADO) {
-            throw new UnsupportedOperationException("Acción no permitida... ");
-        } else if (this.estado == Estado.PARADO) {
-            throw new UnsupportedOperationException("Acción no permitida... ");
-        } else if (this.estado == Estado.PREPARADO) {
-            throw new UnsupportedOperationException("Acción no permitida... ");
-        } else if (this.estado == Estado.ESPERANDO) {
-            if (respuesta == 0) {
-                this.estado = Estado.PREPARADO;
-            } else {
-                this.estado = Estado.CERRADO;
-            }
-        }
-        assert false : "estado imposible";
+        state.recibir(this, respuesta);
+//        if (this.estado == Estado.CERRADO) {
+//            throw new UnsupportedOperationException("Acción no permitida... ");
+//        } else if (this.estado == Estado.PARADO) {
+//            throw new UnsupportedOperationException("Acción no permitida... ");
+//        } else if (this.estado == Estado.PREPARADO) {
+//            throw new UnsupportedOperationException("Acción no permitida... ");
+//        } else if (this.estado == Estado.ESPERANDO) {
+//            if (respuesta == 0) {
+//                this.estado = Estado.PREPARADO;
+//            } else {
+//                this.estado = Estado.CERRADO;
+//            }
+//        }
+//        assert false : "estado imposible";
     }
-
+    
+    protected void setState(State state) {
+        this.state = state;
+    }
 }
