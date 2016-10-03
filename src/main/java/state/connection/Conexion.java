64ac15f37de364193485b09a1708320babdb9680
JacoboCL <j.conejol@alumnos.upm.es>
2016-10-03 19:05:27 +0200

    Create class Waiting. Modified Conexion to create Closed state initially

--- src/main/java/state/connection/Conexion.java
+++ src/main/java/state/connection/Conexion.java
@@ -8,7 +8,7 @@ public class Conexion {
     public Conexion(Link link) {
         assert link != null;
         this.link = link;
-        //this.estado = Estado.CERRADO;
+        state = new Closed();
     }
 
     public Link getLink() {
@@ -21,87 +21,26 @@ public class Conexion {
 
     public void abrir() {
         state.abrir(this);
-//        if (this.estado == Estado.CERRADO) {
-//            this.estado = Estado.PREPARADO;
-//        } else if (this.estado == Estado.PARADO) {
-//            throw new UnsupportedOperationException("Acción no permitida... ");
-//        } else if (this.estado == Estado.PREPARADO) {
-//        } else if (this.estado == Estado.ESPERANDO) {
-//            throw new UnsupportedOperationException("Acción no permitida... ");
-//        } else
-//            assert false : "estado imposible";
     }
 
     public void cerrar() {
         state.cerrar(this);
-//        if (this.estado == Estado.CERRADO) {
-//        } else if (this.estado == Estado.PARADO) {
-//            throw new UnsupportedOperationException("Acción no permitida... ");
-//        } else if (this.estado == Estado.PREPARADO) {
-//            this.estado = Estado.CERRADO;
-//        } else if (this.estado == Estado.ESPERANDO) {
-//            throw new UnsupportedOperationException("Acción no permitida... ");
-//        }
-//        assert false : "estado imposible";
     }
 
     public void parar() {
         state.parar(this);
-//        if (this.estado == Estado.CERRADO) {
-//            throw new UnsupportedOperationException("Acción no permitida... ");
-//        } else if (this.estado == Estado.PARADO) {
-//        } else if (this.estado == Estado.PREPARADO) {
-//            this.estado = Estado.PARADO;
-//        } else if (this.estado == Estado.ESPERANDO) {
-//            throw new UnsupportedOperationException("Acción no permitida... ");
-//        }
-//        assert false : "estado imposible";
     }
 
     public void iniciar() {
         state.iniciar(this);
-//        if (this.estado == Estado.CERRADO) {
-//            throw new UnsupportedOperationException("Acción no permitida... ");
-//        } else if (this.estado == Estado.PARADO) {
-//            this.estado = Estado.PREPARADO;
-//        } else if (this.estado == Estado.PREPARADO) {
-//        } else if (this.estado == Estado.ESPERANDO) {
-//            throw new UnsupportedOperationException("Acción no permitida... ");
-//        }
-//        assert false : "estado imposible";
     }
 
     public void enviar(String msg) {
         state.enviar(this, msg);
-//        if (this.estado == Estado.CERRADO) {
-//            throw new UnsupportedOperationException("Acción no permitida... ");
-//        } else if (this.estado == Estado.PARADO) {
-//            throw new UnsupportedOperationException("Acción no permitida... ");
-//        } else if (this.estado == Estado.PREPARADO) {
-//            this.link.enviar(msg);
-//            this.estado = Estado.ESPERANDO;
-//        } else if (this.estado == Estado.ESPERANDO) {
-//            throw new UnsupportedOperationException("Acción no permitida... ");
-//        }
-//        assert false : "estado imposible";
     }
 
     public void recibir(int respuesta) {
         state.recibir(this, respuesta);
-//        if (this.estado == Estado.CERRADO) {
-//            throw new UnsupportedOperationException("Acción no permitida... ");
-//        } else if (this.estado == Estado.PARADO) {
-//            throw new UnsupportedOperationException("Acción no permitida... ");
-//        } else if (this.estado == Estado.PREPARADO) {
-//            throw new UnsupportedOperationException("Acción no permitida... ");
-//        } else if (this.estado == Estado.ESPERANDO) {
-//            if (respuesta == 0) {
-//                this.estado = Estado.PREPARADO;
-//            } else {
-//                this.estado = Estado.CERRADO;
-//            }
-//        }
-//        assert false : "estado imposible";
     }
     
     protected void setState(State state) {
