ba5072a8e9a5e6c3e157c4f6d8348763ba830669
JacoboCL <j.conejol@alumnos.upm.es>
2016-10-03 18:15:02 +0200

    Create abstract class State.

---
+++ src/main/java/state/connection/State.java
@@ -0,0 +1,18 @@
+package state.connection;
+
+public abstract class State {
+    
+    public abstract Estado getEstado();
+    
+    public abstract void abrir(Conexion conexion);
+    
+    public abstract void cerrar(Conexion conexion);
+    
+    public abstract void parar(Conexion conexion);
+    
+    public abstract void iniciar(Conexion conexion);
+    
+    public abstract void enviar(Conexion conexion, String msg);
+    
+    public abstract void recibir(Conexion conexion, int respuesta);
+}
